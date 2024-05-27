ae9e6a911f6c63f4e3cdc45a48e794b64d403fb7
233191 <233191@students.aih.nsw.edu.au>
2024-05-27 18:40:42 +1000

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,20 @@
+# Write a program to input the age of a user and let them know if they have diabetes risk or not. 
+#Use case is that a user with age more than or equal to 65 is at risk else not.
+age = 55
+if age >= 65
+  print("Risky") 
+else
+  print("Not Risky") 
+
+# WAP to calculate the GST of two products, apple and pie
+# apple has 10% GSt and pie has 8%
+# Apple gross sale is $75, pie gross is $100
+# GST for apple is .10 * 75
+# Gst for pie is .08 * 100\
+
+apple = 75
+pie = 100
+gst_apple = 0.1 * apple
+gst_pie = 0.08 * pie
+print(gst_apple)
+print(gst_pie)
