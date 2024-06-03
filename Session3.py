0466b7fbcfe828fb93b62d2b8afe9e90c342d0d4
233191 <233191@students.aih.nsw.edu.au>
2024-06-03 16:10:21 +1000

    Update Session3.py

--- Session3.py
+++ Session3.py
@@ -1,1 +1,6 @@
-
+# The if else statement
+wthr = input("What's the weather like today (Hot, Cold): ") # get input from the user
+if wthr == "Hot": # check the user input comparing with a string
+    print("Stay home and have cold drinks") # If the above condition is satisfied, print the result
+else:
+    print("Put on some clothes") # If the check condition is not satisfied, print the result
