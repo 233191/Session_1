29dde8303946d4945bb28d1199b1da16bb71d59a
233191 <233191@students.aih.nsw.edu.au>
2024-06-03 16:19:18 +1000

    Update Session3.py

--- Session3.py
+++ Session3.py
@@ -4,3 +4,12 @@ if wthr == "Hot": # check the user input comparing with a string
     print("Stay home and have cold drinks") # If the above condition is satisfied, print the result
 else:
     print("Put on some clothes") # If the check condition is not satisfied, print the result
+
+# The if else statement
+wthr = input("What's the weather Outside (Cloudy, Foggy): ") # get input from the user
+if wthr == "Cloudy": # check the user input comparing with a string
+    print("It's going to rain) # If the above condition is satisfied, print the result
+else:
+    print("Be safe while driving") # If the check condition is not satisfied, print the result
+
+
